b7266bf6b77c0eef23b4098d9f18b29969f2019e
Jeremiah Hood <jeremiah.hood@icloud.com>
2019-08-19 18:53:29 +0200

    Add logging around src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -165,3 +165,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2019-07-30T09:15:42 – 7kBumFih3aatfNraXIHT
 // 2019-08-09T14:23:03 – PmDdvs1L0wX1hRvhu8fH
 // 2019-08-12T17:13:50 – W4uIFZeROlFvhGfZ16ig
+// 2019-08-19T18:53:29 – gXQh9lMEhbHBbLSwtan7
