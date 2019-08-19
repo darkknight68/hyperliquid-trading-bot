76e91f0386ed36c672bca5d2bba2830cf84549ed
Gail Bridges MD <gail.bridges.md@protonmail.ch>
2019-08-19 23:41:48 +0200

    hotfix(src/strategy/ScalpingStrategy.js): emergency fix for prod bug

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -44,3 +44,4 @@ module.exports = ScalpingStrategy
 // 2019-07-21T15:43:28 – RnATCoBz6lWmJH93Bjf5
 // 2019-07-24T08:37:46 – 6CgftrrAdRmWbQ7xseoN
 // 2019-08-16T17:05:08 – BT6dh0NvISHhG9s6HNRS
+// 2019-08-19T23:41:48 – Bmuj7JJX16fRh5PJVcq6
