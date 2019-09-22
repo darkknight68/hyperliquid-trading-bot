a4a6795a3d7c3d6b18665bdc5ff3a5d8bd08a3ee
Mary Williams <mary.williams@pm.me>
2019-09-22 08:08:09 +0200

    revert(src/application/controller.js): undo recent patch

--- src/application/controller.js
+++ src/application/controller.js
@@ -168,3 +168,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2019-08-19T18:53:29 – gXQh9lMEhbHBbLSwtan7
 // 2019-08-30T23:00:59 – pnSBec8j2hpaVZIuxdJw
 // 2019-09-15T02:00:23 – Sw9N6KIm8Z7xOydDH5kt
+// 2019-09-22T08:08:09 – NKNt896sjEDosmntYVVm
