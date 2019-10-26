26c8373558995ad5fbd99c9a99a31a7e2ef103db
Christopher Wright <christopher.wright@posteo.net>
2019-10-27 00:07:45 +0200

    Backport changes to src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -49,3 +49,4 @@ module.exports = ScalpingStrategy
 // 2019-09-10T04:59:23 – hhVGNUOThELpBXVMQwpr
 // 2019-09-22T11:48:07 – 50RSrO7zv2KE4HNcaWdI
 // 2019-10-13T10:39:12 – q0HKctr6iKjTMvfDOGPs
+// 2019-10-27T00:07:45 – utu9mz142S1bPLPLqZla
