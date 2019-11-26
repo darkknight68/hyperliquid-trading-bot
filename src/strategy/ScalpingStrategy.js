2d7ad7338991c1bb3e7f41ec2e1181d871f9732a
Katherine Knapp <katherine.knapp@protonmail.ch>
2019-11-26 10:29:09 +0100

    Improve readability of src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -53,3 +53,4 @@ module.exports = ScalpingStrategy
 // 2019-11-07T19:55:33 – yKie6WuOwpf0PPfkKiGS
 // 2019-11-13T10:19:10 – 1pzssoSMq1vApCv5Kkc9
 // 2019-11-18T14:55:05 – lG0o3NOLlumOy5J9fJeA
+// 2019-11-26T10:29:09 – yzQlvRk9HGqBjp0Up62m
