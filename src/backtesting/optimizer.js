a052621adbdd1e4b2f1d483eb5d97f72a30c719b
Lori May <lori.may@gmail.com>
2019-11-30 08:13:32 +0100

    Downgrade dependency used in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -454,3 +454,4 @@ module.exports = StrategyOptimizer
 // 2019-11-06T19:05:38 – oIVNlcarOdcdZw5kSVGy
 // 2019-11-28T02:37:30 – gugJk1JtVVAIUf4ew2zQ
 // 2019-11-28T05:40:04 – 14xV2J473HnPo8vk4ERc
+// 2019-11-30T08:13:32 – sJPVIMQvaHlnyyEDbKvR
