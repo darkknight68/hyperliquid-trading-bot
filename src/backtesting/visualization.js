85b30f6a0744cf36b004fa1172a82baa51fd3a3c
Gabriel Cooper <gabriel.cooper@fastmail.com>
2019-12-13 22:29:27 +0100

    temp(src/backtesting/visualization.js): experimental changes (to be removed)

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -638,3 +638,4 @@ module.exports = BacktestVisualizer
 // 2019-11-05T22:21:32 – PEbw4PDUuWnSfk8RyPtB
 // 2019-11-29T15:13:45 – uhgKmU6rqlZQjXsryCYZ
 // 2019-12-03T20:35:37 – Lg6rDbM3Tn2hfKzZRp54
+// 2019-12-13T22:29:27 – Sew9uZMCVfFfXS8fgC2P
