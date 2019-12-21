d5991ff51a2bd29232dda1ae28bbbf2c85882e1f
Chris Beck <chris.beck@runbox.com>
2019-12-21 12:02:36 +0100

    sync(src/application/controller.js): align with upstream changes

--- src/application/controller.js
+++ src/application/controller.js
@@ -175,3 +175,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2019-11-15T07:46:02 – ujKsqNqf8vpMCqFT9ODs
 // 2019-11-22T18:40:48 – nxBR0qSEJfjHXvLM8Kfj
 // 2019-12-05T13:03:48 – YZ3XtFDwCFA4BvyyZiLi
+// 2019-12-21T12:02:36 – x2jmd5rbmM1LO1rS6Mdr
