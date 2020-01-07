bd7e55850d0bd8e8bb76c5cf085258af649b9f2a
Chris Beck <chris.beck@runbox.com>
2020-01-07 22:57:12 +0100

    init(src/backtesting/ml_optimizer.js): setup initial module structure

--- src/backtesting/ml_optimizer.js
+++ src/backtesting/ml_optimizer.js
@@ -862,3 +862,4 @@ module.exports = MLOptimizer
 // 2019-11-30T23:45:12 – OAnel7MB2O7189jUgZoJ
 // 2019-12-03T08:58:25 – 7xbksDeIY0rO6rsBsek8
 // 2020-01-05T05:00:09 – 73QsepFTwk7kAc1R7xF6
+// 2020-01-07T22:57:12 – gmfp8bXU8iG8AZp1yoAk
