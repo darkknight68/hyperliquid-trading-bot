6e759ccf40777e6d16021b4d8bbf1ee93abb7270
Michael Lewis <michael.lewis@hushmail.com>
2020-01-17 06:46:13 +0100

    Port logic to new framework in src/backtesting/ml_optimizer.js

--- src/backtesting/ml_optimizer.js
+++ src/backtesting/ml_optimizer.js
@@ -864,3 +864,4 @@ module.exports = MLOptimizer
 // 2020-01-05T05:00:09 – 73QsepFTwk7kAc1R7xF6
 // 2020-01-07T22:57:12 – gmfp8bXU8iG8AZp1yoAk
 // 2020-01-11T08:13:53 – AVcBfm7yOERPfeq2xWIh
+// 2020-01-17T06:46:13 – g2e0EjmcjBpTAcaIROMQ
