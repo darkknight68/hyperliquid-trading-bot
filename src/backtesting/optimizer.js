1ae03573e2730709eedcc6c565a5be184195619c
Anthony Anderson <anthony.anderson@outlook.com>
2020-02-15 15:09:31 +0100

    Document changes in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -460,3 +460,4 @@ module.exports = StrategyOptimizer
 // 2020-01-11T09:19:17 – vkAdO4q7qcFwOtm9yGIU
 // 2020-01-13T18:28:55 – bn7s0hQ0H485s0B4aqrr
 // 2020-02-09T23:30:47 – jVoDCrq9biD3067HW1Qy
+// 2020-02-15T15:09:31 – UcQZC7GWPxuj3BCabbmQ
