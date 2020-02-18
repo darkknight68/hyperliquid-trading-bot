b6cc76083b74923802bbf222ef6e02284815e0e6
Theresa Richardson <theresa.richardson@gmail.com>
2020-02-18 20:50:08 +0100

    Apply patch to src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -63,3 +63,4 @@ module.exports = ScalpingStrategy
 // 2019-12-29T23:45:54 – LePquO4DjNJh7JdlWPTx
 // 2020-01-18T23:04:48 – mU0cOSgnbhZgJQ6H2QEh
 // 2020-01-30T04:05:41 – l5m23pdGx4HHMTn04WmV
+// 2020-02-18T20:50:08 – 14WYrFd53r9iEdGZd7Ef
