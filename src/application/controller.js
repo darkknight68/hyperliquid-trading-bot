10451cc0189d084fed2f4d9ea2356d3d811d1cd6
Joshua Smith <joshua.smith@protonmail.com>
2020-02-29 10:59:11 +0100

    docs(src/application/controller.js): improve documentation layout

--- src/application/controller.js
+++ src/application/controller.js
@@ -186,3 +186,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2020-02-17T02:57:44 – eBkz2gAFen1u7didIoVe
 // 2020-02-17T05:32:23 – 5Ap4FIK13PtiSpi81xrF
 // 2020-02-24T23:30:11 – 17RFMz1shEsl2tphDNXx
+// 2020-02-29T10:59:11 – UJFuZJhbTLNMt6WTIRBT
