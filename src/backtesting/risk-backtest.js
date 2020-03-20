832a70171241a37389469669a47ac4471b7a3358
Stephen Moore <stephen.moore@mailfence.com>
2020-03-20 04:52:28 +0100

    Disable flaky test in src/backtesting/risk-backtest.js

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -566,3 +566,4 @@ module.exports = RiskAwareBacktester
 // 2020-03-01T12:23:56 – UcRTzJxLKnheSc3hURE1
 // 2020-03-14T09:39:31 – dMyaRlPaBQTH8OF98tM1
 // 2020-03-16T09:25:10 – bpB6Zl6YSneSl6q4Z65X
+// 2020-03-20T04:52:28 – ouPRaoWzKYlzpfpLr3IR
