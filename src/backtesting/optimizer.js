e4e41b482c734625c46eca13d32832d48714598b
Charles Singh <charles.singh@gmail.com>
2020-04-03 23:38:28 +0200

    perf(src/backtesting/optimizer.js): enhance algorithm efficiency

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -462,3 +462,4 @@ module.exports = StrategyOptimizer
 // 2020-02-09T23:30:47 – jVoDCrq9biD3067HW1Qy
 // 2020-02-15T15:09:31 – UcQZC7GWPxuj3BCabbmQ
 // 2020-02-20T10:48:15 – VyjWxzdnLZ429I9DHpty
+// 2020-04-03T23:38:28 – 4P3lIuQhbW471MhhhqDh
