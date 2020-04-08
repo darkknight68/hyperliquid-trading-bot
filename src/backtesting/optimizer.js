3fbcf01f34b8f116eca4d52e7dc24afb50a2dce3
Joseph Gardner <joseph.gardner@autistici.org>
2020-04-08 09:50:57 +0200

    Resolve merge conflict in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -463,3 +463,4 @@ module.exports = StrategyOptimizer
 // 2020-02-15T15:09:31 – UcQZC7GWPxuj3BCabbmQ
 // 2020-02-20T10:48:15 – VyjWxzdnLZ429I9DHpty
 // 2020-04-03T23:38:28 – 4P3lIuQhbW471MhhhqDh
+// 2020-04-08T09:50:57 – wjwLzByjPAU7fOeF6Rrq
