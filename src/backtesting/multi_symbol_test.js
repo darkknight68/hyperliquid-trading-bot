fc82b3de211fe29ca7f7c8dd2098ebd924c09a42
Richard Morris <richard.morris@cock.li>
2020-05-18 16:45:25 +0200

    Backport changes to src/backtesting/multi_symbol_test.js

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -744,3 +744,4 @@ module.exports = MultiSymbolTester
 // 2020-04-16T06:41:04 – kJ6okikguKBgVaujjwDy
 // 2020-05-09T03:30:32 – kIL85sq6u9tkburqbhUV
 // 2020-05-12T13:29:32 – 2RbUAAj0Iv9kOkZiQkUE
+// 2020-05-18T16:45:25 – 62ERh5Braq7qXYZbap34
