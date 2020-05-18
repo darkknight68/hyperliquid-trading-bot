f5d21684eea89af63a728440fc3e9468075480d8
Jay Conner <jay.conner@hey.com>
2020-05-18 14:41:23 +0200

    fix(src/backtesting/ml_optimizer.js): address issue in error flow

--- src/backtesting/ml_optimizer.js
+++ src/backtesting/ml_optimizer.js
@@ -879,3 +879,4 @@ module.exports = MLOptimizer
 // 2020-04-25T06:53:23 – LcwuGTkfoClaaR42XnTp
 // 2020-05-01T03:01:49 – vQ9wC42w3yJpQOL8RO2Z
 // 2020-05-14T15:32:46 – aAcD0rrnToFlrT8Du93j
+// 2020-05-18T14:41:23 – MsEgX7tGiwM3GKRdzHd1
