f3d1d785eee6a74173e1eadc1146a83ce329b3cc
Kevin Johnson <kevin.johnson@protonmail.com>
2020-05-30 10:39:43 +0200

    Apply patch to src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -192,3 +192,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2020-04-12T05:30:48 – zGDn8IoL3PqSTtBbAXZp
 // 2020-05-13T22:03:13 – J5Tk9Xr2HAVnDXt8sXgZ
 // 2020-05-25T23:44:39 – Gp5UrmDSRwpR0hRwg48O
+// 2020-05-30T10:39:43 – nIiIaAVJ7fvYDcqF0SIS
