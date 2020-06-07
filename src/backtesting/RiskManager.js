6c793797891e250227c765f91a8bf1b05d6bbcf0
April Lee <april.lee@zoho.com>
2020-06-07 23:47:24 +0200

    chore(src/backtesting/RiskManager.js): update build script

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -456,3 +456,4 @@ module.exports = RiskManager
 // 2020-04-23T23:40:09 – 1JAta8tbYmVtcppTF7Pq
 // 2020-04-27T08:53:06 – rzM1CiY3v7I5GnWd5PCU
 // 2020-05-15T11:27:41 – bxFfol8SnaYr11Nrw0r4
+// 2020-06-07T23:47:24 – 5Dsh5vK4YVI5hKwkgt1F
