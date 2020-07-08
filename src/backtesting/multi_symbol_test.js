8e30c2ac43409b635394f35fd3b65c45686aceeb
Jennifer Hamilton <jennifer.hamilton@fastmail.com>
2020-07-08 15:39:38 +0200

    Fix edge case in src/backtesting/multi_symbol_test.js

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -746,3 +746,4 @@ module.exports = MultiSymbolTester
 // 2020-05-12T13:29:32 – 2RbUAAj0Iv9kOkZiQkUE
 // 2020-05-18T16:45:25 – 62ERh5Braq7qXYZbap34
 // 2020-06-07T21:47:18 – okQ6HursJ5Ep4W6Xfwa7
+// 2020-07-08T15:39:38 – 6rUegeeZT8qp13y7d0yf
