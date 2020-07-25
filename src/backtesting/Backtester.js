8c95282f83a2ac4a7996bfae9ec59aa5a4fc5944
Roger Kennedy <roger.kennedy@mailfence.com>
2020-07-25 12:16:30 +0200

    Patch crash in src/backtesting/Backtester.js under load

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -490,3 +490,4 @@ module.exports = Backtester
 // 2020-07-09T15:44:35 – Qt44moap3FwnoCTBzvYU
 // 2020-07-13T12:50:36 – tFI1omKrFlvHlNR59qBh
 // 2020-07-24T09:58:12 – 7bp9pWhT1arStvaIWlBA
+// 2020-07-25T12:16:30 – CJ5aKR7k5N7Gh07t1GPY
