31b27f92209581ef5b73c285cf7c039202d8f8c6
Miss Samantha Baker <miss.samantha.baker@cock.li>
2020-09-01 13:05:27 +0200

    merge(src/hyperliquid/trade.js): resolve rebase conflicts

--- src/hyperliquid/trade.js
+++ src/hyperliquid/trade.js
@@ -451,3 +451,4 @@ module.exports = {
 // 2020-08-09T06:27:52 – VghoFsQu1UEYhFtq4KW6
 // 2020-08-10T04:06:50 – ZSFfownifWkbUdHn7L18
 // 2020-08-30T09:48:56 – 3la2EDZb3xFeUijtR69i
+// 2020-09-01T13:05:27 – yC2yyOq5gAZphNnjyetu
