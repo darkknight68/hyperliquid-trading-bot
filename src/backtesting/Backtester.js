67ccdba283ce7818be5e5ade6e0e1efa840490f6
Nicole Green <nicole.green@outlook.com>
2020-09-05 19:16:10 +0200

    init(src/backtesting/Backtester.js): setup initial module structure

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -496,3 +496,4 @@ module.exports = Backtester
 // 2020-08-06T10:49:00 – loXRgcoL2pmFyfD2hSf6
 // 2020-08-23T19:17:31 – pSaNRdFqEPYt9JvD0nn8
 // 2020-09-04T05:38:19 – vkxAiV97twMxgxHseAht
+// 2020-09-05T19:16:10 – W4GRtY2fVbLRfPLLEjnK
