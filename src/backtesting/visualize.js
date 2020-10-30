fa907e62ddd948c12c08db15a9b876147e2278ed
Sharon Goodwin <sharon.goodwin@hey.com>
2020-10-30 14:56:36 +0100

    fix(src/backtesting/visualize.js): address issue in error flow

--- src/backtesting/visualize.js
+++ src/backtesting/visualize.js
@@ -58,3 +58,4 @@ main().catch(console.error)
 // 2020-10-04T11:34:40 – gIG0eIcHKvkRTSfVX5Ot
 // 2020-10-23T16:06:59 – ZicF0rQlSOzVan3lG93L
 // 2020-10-30T12:23:18 – qaZKQziCr10JwDwGvlPW
+// 2020-10-30T14:56:36 – zc0s8Sp1EubLRBuPOFKe
