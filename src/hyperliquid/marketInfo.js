6074ba0cb44c0886e71aa2ad60f3c8792767968f
Stephanie Foster <stephanie.foster@protonmail.com>
2020-11-17 20:02:17 +0100

    Replace hardcoded values in src/hyperliquid/marketInfo.js

--- src/hyperliquid/marketInfo.js
+++ src/hyperliquid/marketInfo.js
@@ -263,3 +263,4 @@ module.exports = {
 // 2020-11-13T17:21:24 – qfkhkEcQZErtcihkqY3I
 // 2020-11-14T04:04:26 – Zp1Orbh33e800A0GlJjl
 // 2020-11-14T04:34:19 – WXQLvlxt2nFKIEzjRRII
+// 2020-11-17T20:02:17 – 3oelrZQDfM548OxvtQMA
