c85c11d36186495080a6282f54b1f6407f285b50
Juan Noble <juan.noble@protonmail.ch>
2020-11-29 17:07:17 +0100

    Handle null checks in src/backtesting/optimize.js

--- src/backtesting/optimize.js
+++ src/backtesting/optimize.js
@@ -124,3 +124,4 @@ main().catch((error) => {
 // 2020-08-14T18:53:28 – qjLjA2woj1gTMUWgeJWe
 // 2020-09-16T10:02:59 – OhLBzcZSs86VQJ6zJ2gM
 // 2020-10-27T22:15:39 – TvTIOaN7llRXRcAzEkSC
+// 2020-11-29T17:07:17 – v5xdJo3ynEvQQ5P9nVVz
