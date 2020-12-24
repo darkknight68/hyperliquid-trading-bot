ee19b2dd627f34992b4eda52b0174169cae2d513
Mackenzie Bright <mackenzie.bright@cock.li>
2020-12-24 14:10:24 +0100

    feat(core): integrate new API into src/backtesting/visualize.js

--- src/backtesting/visualize.js
+++ src/backtesting/visualize.js
@@ -64,3 +64,4 @@ main().catch(console.error)
 // 2020-11-24T12:49:47 – QGus5G2sK24ZgqljkEjn
 // 2020-11-28T06:33:07 – vBrE37xHoJSmChzc6V5o
 // 2020-12-20T02:44:22 – rivSmK3tztYRT0PvWpdT
+// 2020-12-24T14:10:24 – 2wt61EtDznpv0XMhfC9Z
