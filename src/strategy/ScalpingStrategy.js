adefd2d39c13d6335dbdb4a8ef3b7235c7672cc1
William Brown <william.brown@posteo.net>
2020-12-30 13:24:04 +0100

    fix(src/strategy/ScalpingStrategy.js): address issue in error flow

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -94,3 +94,4 @@ module.exports = ScalpingStrategy
 // 2020-12-09T00:29:51 – 6OmbXRllXP8vd3FZk32I
 // 2020-12-14T13:24:58 – o22S5OlkJQ6DSvKHiapM
 // 2020-12-26T13:14:27 – lUp9Smk4KopJtMoViPI5
+// 2020-12-30T13:24:04 – 03GkCzZTqtH7hlx3cTcv
