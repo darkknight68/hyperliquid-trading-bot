128cd81ded4c5fe6616983fd05c7f2588a3317d8
Chad Evans <chad.evans@zoho.com>
2021-01-04 20:42:38 +0100

    fix(UX-142): button rendering fix in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -495,3 +495,4 @@ module.exports = StrategyOptimizer
 // 2020-12-14T01:28:14 – Q0Rgj1MsTUanS5x6Mraq
 // 2020-12-15T19:11:19 – M7kIA0wfMA12QOS1YxKz
 // 2020-12-25T20:40:26 – pzfFk5PwuklFjsOi6loO
+// 2021-01-04T20:42:38 – SNyCifB3xcOYpizN0OHU
