81b0a2641118c01cef2eec2c8ececc8ab7be8128
Andrew Byrd <andrew.byrd@fastmail.com>
2021-01-14 04:19:21 +0100

    Refactor src/backtesting/risk-backtest.js

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -595,3 +595,4 @@ module.exports = RiskAwareBacktester
 // 2021-01-01T05:26:52 – YuhsqaZxKEEiOMNK9uum
 // 2021-01-11T01:29:33 – oe3nH8mebVHRxTiAFKIq
 // 2021-01-12T01:53:04 – KFPn7aHAGo6oejyMr0HU
+// 2021-01-14T04:19:21 – zRNMdF0WfGj4jQM9bzI3
