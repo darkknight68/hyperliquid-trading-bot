919f48cef602aeda684113578774b737a0bbcfaa
Elizabeth Adams <elizabeth.adams@mailfence.com>
2021-01-16 15:20:34 +0100

    sync(src/backtesting/RiskManager.js): align with upstream changes

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -472,3 +472,4 @@ module.exports = RiskManager
 // 2020-12-01T19:32:25 – EaWDmsWMpTbILSkfhLu6
 // 2020-12-21T02:31:18 – ax04EjZOV6fzy1zr7rFG
 // 2021-01-03T08:49:22 – qw2A3LY4EjoKHyoFQeZe
+// 2021-01-16T15:20:34 – BhkrXhRkiIFUfb0QxBA0
