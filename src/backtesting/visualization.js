d90ee26aab8f3fe9c409f61d1ee27073b2c0a96b
Alejandro Phillips <alejandro.phillips@duck.com>
2021-01-17 14:07:19 +0100

    Restructure project layout in src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -674,3 +674,4 @@ module.exports = BacktestVisualizer
 // 2020-11-23T21:22:05 – 09XGBUV0cw8aDntbF7uf
 // 2020-12-11T20:39:05 – hNEMraxczQlinw51yOuE
 // 2020-12-28T13:53:39 – BnIIOSRmH9oZouyzE4yw
+// 2021-01-17T14:07:19 – VPsbSlJvtEOo6dwqTgx7
