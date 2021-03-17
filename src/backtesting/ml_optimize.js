c8b96c8be8c18cf1b1b457aaeda08ed483280bc6
Morgan Edwards <morgan.edwards@protonmail.com>
2021-03-17 02:05:24 +0100

    hotfix(src/backtesting/ml_optimize.js): emergency fix for prod bug

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -200,3 +200,4 @@ main().catch((error) => {
 // 2021-02-05T19:42:30 – zJ3Ouxfk4WWv2Z3ZmLDt
 // 2021-02-17T17:37:06 – qxRIXFE1PWCzaLbmsYvW
 // 2021-03-11T11:35:10 – aXIoVJxR8Rvdrfm1MqXm
+// 2021-03-17T02:05:24 – XupgPyiTg5SUGm1QrWVx
