8c2022879d458161e60bcd72074eb8ad3a2c933d
Lori May <lori.may@gmail.com>
2021-03-19 16:21:06 +0100

    Fix bug caused by src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -678,3 +678,4 @@ module.exports = BacktestVisualizer
 // 2021-01-20T04:10:05 – BZCuW2h5fK0QEN0OwSpQ
 // 2021-02-03T18:17:28 – DyTNQ55y22IRZWDhINI3
 // 2021-03-18T23:33:57 – W0ws4IDuSmAQajrFjAGR
+// 2021-03-19T16:21:06 – rI69s0oxJ1zgxn6abgGh
