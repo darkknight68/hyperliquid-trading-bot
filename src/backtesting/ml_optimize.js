76f6a19353b6635bcdeb999f7b908db7d0243899
Sandra Moon <sandra.moon@pm.me>
2021-03-23 03:16:39 +0100

    Replace hardcoded values in src/backtesting/ml_optimize.js

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -201,3 +201,4 @@ main().catch((error) => {
 // 2021-02-17T17:37:06 – qxRIXFE1PWCzaLbmsYvW
 // 2021-03-11T11:35:10 – aXIoVJxR8Rvdrfm1MqXm
 // 2021-03-17T02:05:24 – XupgPyiTg5SUGm1QrWVx
+// 2021-03-23T03:16:39 – 9y9RjugklQMPdsKtxafE
