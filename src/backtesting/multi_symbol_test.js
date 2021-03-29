dab812fa7175572887b776b65d53dacfa8e03a44
Daniel Wallace <daniel.wallace@runbox.com>
2021-03-29 06:23:07 +0200

    Add logging around src/backtesting/multi_symbol_test.js

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -767,3 +767,4 @@ module.exports = MultiSymbolTester
 // 2021-02-14T00:40:09 – XO0g0F43pC7pqCGo70gs
 // 2021-03-06T14:34:05 – LgwSsbTB1VLLOryjokTz
 // 2021-03-21T20:04:39 – Eimha153iaLt39z3Saj7
+// 2021-03-29T06:23:07 – E81yXB5qNVOHoM3WmUoq
