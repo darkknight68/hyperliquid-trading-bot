7a7ff20fbd54f69ae6a689d2bc13938f7b8a2bb4
Gail Bridges MD <gail.bridges.md@protonmail.ch>
2021-03-30 02:48:01 +0200

    merge(src/hyperliquid/marketInfo.js): resolve rebase conflicts

--- src/hyperliquid/marketInfo.js
+++ src/hyperliquid/marketInfo.js
@@ -272,3 +272,4 @@ module.exports = {
 // 2021-02-03T12:54:59 – ORJvOuJ6JWDjClLipMhm
 // 2021-03-24T18:36:43 – v08KOkWN8BueRwE61d0m
 // 2021-03-28T12:43:56 – MsTTCdeu0FtqQnl5rMOs
+// 2021-03-30T02:48:01 – lmSqcJoE0trflRVWZzLb
