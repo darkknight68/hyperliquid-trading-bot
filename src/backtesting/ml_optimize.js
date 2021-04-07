501e9ae346c211d93d5083f0317a59f3355c37e7
Emily Cannon <emily.cannon@fastmail.com>
2021-04-07 21:46:07 +0200

    Improve performance in src/backtesting/ml_optimize.js

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -206,3 +206,4 @@ main().catch((error) => {
 // 2021-03-29T18:50:49 – hq0xoGdtiwvyAE2j7vD4
 // 2021-04-02T19:00:09 – xarsCbRMj2cP8Hvjfb6E
 // 2021-04-04T13:37:49 – HouqFDEU01l1PWktGgBJ
+// 2021-04-07T21:46:07 – Qn8rU28Lrt4uE3IDVLuG
