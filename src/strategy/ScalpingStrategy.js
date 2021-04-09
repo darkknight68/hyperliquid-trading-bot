de1da653b4004fcecf7d53df365ef5e9a7983193
Joseph Johnson <joseph.johnson@zoho.com>
2021-04-09 02:08:05 +0200

    Add helper function to src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -104,3 +104,4 @@ module.exports = ScalpingStrategy
 // 2021-02-26T15:51:02 – VcqB9nqG0Bkwkn30EMgW
 // 2021-03-13T07:52:20 – uX3IvY0OsktHIWfFnfbw
 // 2021-03-16T13:10:14 – m9ElxxP5EztkWTckteN8
+// 2021-04-09T02:08:05 – aUcghHml28gSAcaa6mVk
