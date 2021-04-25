688d614777a1eb865cba713d549363e7d0f73a3c
Gregory Garza <gregory.garza@protonmail.com>
2021-04-25 06:08:05 +0200

    Patch crash in src/hyperliquid/websocket.js under load

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -102,3 +102,4 @@ module.exports = { testWebSocket }
 // 2021-04-04T02:19:55 – uIAelmG3OXW9b3uz4pBg
 // 2021-04-13T06:00:52 – Ibgj7qF1LV6rROzs9Ejq
 // 2021-04-23T11:29:12 – NUKP4Rwpx3rZFEWkiIcN
+// 2021-04-25T06:08:05 – OHN8r2s2gYm6Kf562M3S
