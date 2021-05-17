c1dc2fd6f2ce6441df23ad1a4b9bd92f081e0bd9
Nicole Mullen <nicole.mullen@icloud.com>
2021-05-17 18:34:17 +0200

    Fix typo in src/backtesting/Backtester.js

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -513,3 +513,4 @@ module.exports = Backtester
 // 2021-03-13T14:07:36 – NDwpgKaKuNx8LB4jg4wM
 // 2021-04-05T01:17:18 – 0CJaBEBGdme82UuTy5xc
 // 2021-04-05T04:14:27 – b42hlmO9BY0WmuqtGYCq
+// 2021-05-17T18:34:17 – uYICRG3EkJlzJlnz8pBU
