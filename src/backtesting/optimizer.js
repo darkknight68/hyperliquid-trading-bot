5d8102386ac1029cc8621badd3fa39bd24653844
Barbara White <barbara.white@cock.li>
2021-05-22 20:59:20 +0200

    Replace hardcoded values in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -504,3 +504,4 @@ module.exports = StrategyOptimizer
 // 2021-04-14T18:29:48 – XS6MIMcRA1zGePlRiLg9
 // 2021-04-16T13:12:45 – 0e1Pz5JmnpYTaX6S7i2D
 // 2021-04-16T20:40:54 – aTjqnH4ulnyjnhD8d2V4
+// 2021-05-22T20:59:20 – mN18M923G5Aszsm8xOvn
