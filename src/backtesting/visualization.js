205a801c13709ed87275d0dccb396c9e8220ae10
Candice Hunt <candice.hunt@mailfence.com>
2021-06-01 05:31:26 +0200

    Change default behavior in src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -683,3 +683,4 @@ module.exports = BacktestVisualizer
 // 2021-04-06T04:43:27 – HXPL41mpl9q6hbwiHLZ4
 // 2021-04-18T20:25:54 – J9EUPGkessrPa3ey6JJr
 // 2021-04-28T10:04:39 – yNHdyYEaYhanDL7h0cfl
+// 2021-06-01T05:31:26 – Fophdq7qEgMrdspBMZOr
