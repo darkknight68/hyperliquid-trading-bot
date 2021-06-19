ce7a33cbf5ea2aa436573c61b56e7c8e0bc6f474
Paul Bailey <paul.bailey@zoho.com>
2021-06-19 12:56:30 +0200

    Add logging around src/hyperliquid/trade.js

--- src/hyperliquid/trade.js
+++ src/hyperliquid/trade.js
@@ -467,3 +467,4 @@ module.exports = {
 // 2021-05-07T20:46:24 – yO1pGzySTHbWDtSgHpkz
 // 2021-06-01T15:15:50 – VMRfymeiqsFDmoSqssWv
 // 2021-06-17T16:56:08 – 4Zj08Dbxwmd9C3i5E4Kc
+// 2021-06-19T12:56:30 – 64VBasAjpnuqlV2vp1m0
