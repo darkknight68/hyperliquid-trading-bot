21826b3ae04b6c259ace62a678200dd99427533b
John Ibarra <john.ibarra@gmail.com>
2021-08-04 02:53:12 +0200

    docs(src/strategy/indicators/index.js): improve documentation layout

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -148,3 +148,4 @@ module.exports = {
 // 2021-07-15T08:50:48 – zmvcqCI5maoM043DpmFq
 // 2021-07-29T07:47:14 – Vi8iSfPxQ7HJC8efKkEa
 // 2021-08-02T03:56:41 – WFRqpDsPJ7WKkoNhk9Cn
+// 2021-08-04T02:53:12 – Ju7lfGpnSQiJ7P5rVBVf
