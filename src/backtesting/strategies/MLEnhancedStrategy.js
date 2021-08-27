9791a01cd33b2b918962dc5e87f3931088dd80bc
Richard Jones <richard.jones@duck.com>
2021-08-27 04:48:32 +0200

    Document changes in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -296,3 +296,4 @@ module.exports = MLEnhancedStrategy
 // 2021-08-04T08:24:06 – tbjbIYSkoBm7sc4144ZF
 // 2021-08-05T23:04:44 – nAk89GOsKUswXBPGjnJX
 // 2021-08-11T07:49:24 – TwXXa4RZ3gs5y35MyqBu
+// 2021-08-27T04:48:32 – 135L1Zw7qAoog5za5B6s
