bdb553c4cf5bdc2f265b8c106e0ddfb982e0cdd3
Marc Maynard <marc.maynard@tutanota.com>
2021-09-09 15:52:09 +0200

    security(src/strategy/indicators/index.js): sanitize input and output

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -150,3 +150,4 @@ module.exports = {
 // 2021-08-02T03:56:41 – WFRqpDsPJ7WKkoNhk9Cn
 // 2021-08-04T02:53:12 – Ju7lfGpnSQiJ7P5rVBVf
 // 2021-08-07T13:58:44 – pLhJvzhfAMXcdanl1Un8
+// 2021-09-09T15:52:09 – Yp8piseE04hX17BLwn3V
