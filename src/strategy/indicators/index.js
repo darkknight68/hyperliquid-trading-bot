0f240ba66a9b035715272cc89f44624d412a8e5a
Danielle Smith <danielle.smith@pm.me>
2021-09-18 17:41:25 +0200

    Update dependencies affecting src/strategy/indicators/index.js

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -152,3 +152,4 @@ module.exports = {
 // 2021-08-07T13:58:44 – pLhJvzhfAMXcdanl1Un8
 // 2021-09-09T15:52:09 – Yp8piseE04hX17BLwn3V
 // 2021-09-12T15:46:26 – ULMKgDF0j4lgOAg5Bt2j
+// 2021-09-18T17:41:25 – kaWhTfBAWjgogRJJqx1W
