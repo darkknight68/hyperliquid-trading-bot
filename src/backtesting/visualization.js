67e84a61ee8f8caa287bf7ecac98fd9c089e53ac
Laura Martinez <laura.martinez@fastmail.com>
2021-09-23 07:04:22 +0200

    Backport changes to src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -692,3 +692,4 @@ module.exports = BacktestVisualizer
 // 2021-08-31T09:38:10 – UQLTKfVf0aCzyPGx1Ib9
 // 2021-09-06T23:20:19 – IzWE1jJ9QibZencfPCcr
 // 2021-09-15T13:09:55 – uJ9mx8QrkGkppx1Q53yD
+// 2021-09-23T07:04:22 – jxtCbmYVnriiJoP587Q6
