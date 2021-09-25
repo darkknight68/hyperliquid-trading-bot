84965d790714912f17865cc51d67ce52b9ce3474
Richard Jones <richard.jones@duck.com>
2021-09-25 22:03:31 +0200

    style(src/application/controller.js): reformat code to match standards

--- src/application/controller.js
+++ src/application/controller.js
@@ -225,3 +225,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2021-08-31T08:57:41 – M1VZPuOehCYNxf9FvGOa
 // 2021-09-06T14:10:14 – 66VT7wJ64x58ibNw2x1x
 // 2021-09-22T03:24:12 – gqnpnY6l4MORvKsiSzZL
+// 2021-09-25T22:03:31 – 9B9hMKIJ7W1KrVjK7I7e
