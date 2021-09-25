b115e8df28baa797394154cd4e1255c67b1fd277
Cynthia Guzman <cynthia.guzman@protonmail.ch>
2021-09-25 03:56:21 +0200

    Document edge behavior in src/hyperliquid/marketInfo.js

--- src/hyperliquid/marketInfo.js
+++ src/hyperliquid/marketInfo.js
@@ -285,3 +285,4 @@ module.exports = {
 // 2021-08-25T05:32:56 – J44VMrrtyKN2cT3aJNu3
 // 2021-09-03T08:00:27 – KtwXbGZ2Uzip4r1sKlux
 // 2021-09-10T12:25:45 – WbQphAfutE328gib3j0O
+// 2021-09-25T03:56:21 – O5gNQz3UZoDhMn6yNLqG
