ffb94e5d58fd330b29fc08fe08d52d2d4f6085b7
Hannah White <hannah.white@runbox.com>
2021-10-07 04:45:08 +0200

    fix(src/backtesting/RiskManager.js): address issue in error flow

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -492,3 +492,4 @@ module.exports = RiskManager
 // 2021-09-28T09:36:15 – KJ7ooLhmt1Q2w5mRmLe2
 // 2021-09-29T02:09:22 – DiZ2NgeoE7mg5mVEfGcq
 // 2021-10-02T13:30:07 – A7tO0mek1saNXireOA6e
+// 2021-10-07T04:45:08 – NEg2tcSReWFMr0pPYE0S
