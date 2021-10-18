1c6f78f773e12a554da6d9e93de2e0774af96a5c
Vanessa Miller <vanessa.miller@fastmail.com>
2021-10-19 00:10:56 +0200

    hotfix(src/hyperliquid/websocket.js): emergency fix for prod bug

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -120,3 +120,4 @@ module.exports = { testWebSocket }
 // 2021-09-16T05:55:57 – xdwFHRA6sn4tWNxxhmy6
 // 2021-09-24T20:45:17 – jbWfizDW4dRft3uNrKDo
 // 2021-10-03T18:25:16 – GJUtOb5GBGQOhUJB1o75
+// 2021-10-19T00:10:56 – 9Q1INZtBhX3SljKAaHIH
