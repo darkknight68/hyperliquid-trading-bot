72544cd85fd735106a81f212f27b2a3d3b755e2d
Kathryn Smith <kathryn.smith@zoho.com>
2021-10-23 22:31:35 +0200

    Improve performance in src/strategy/indicators/index.js

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -157,3 +157,4 @@ module.exports = {
 // 2021-10-05T18:11:16 – bh6L0Kb28UcoxdxBI3mX
 // 2021-10-08T15:16:12 – ITZTmwoPdfUQUHaxe4xi
 // 2021-10-13T08:31:11 – KWz4Y6SiICmEeH5mixKB
+// 2021-10-23T22:31:35 – pCOISRh4g4OHOkdkyszH
