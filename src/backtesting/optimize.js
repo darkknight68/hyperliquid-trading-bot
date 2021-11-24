56db5685916dcf3bfe49966f205d24525cc19395
Mark Garcia <mark.garcia@hushmail.com>
2021-11-24 13:49:34 +0100

    docs(src/backtesting/optimize.js): improve documentation layout

--- src/backtesting/optimize.js
+++ src/backtesting/optimize.js
@@ -159,3 +159,4 @@ main().catch((error) => {
 // 2021-11-09T02:04:32 – fqccdBNZdveiJ3ZD1Kbq
 // 2021-11-14T09:29:35 – Bhmun8nnXJUgL9ouGyS7
 // 2021-11-24T13:21:50 – 3SMlyWJWzvt6UJglVN9S
+// 2021-11-24T13:49:34 – 4LLtnThNdqVKxo1WhVS5
