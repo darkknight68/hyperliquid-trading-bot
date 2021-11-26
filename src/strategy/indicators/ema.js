07a728138c6d409e6bd7bc1bfbe80a5d68f755f9
Brent Estrada <brent.estrada@pm.me>
2021-11-27 00:25:35 +0100

    feat(DASH-88): add dashboard filters in src/strategy/indicators/ema.js

--- src/strategy/indicators/ema.js
+++ src/strategy/indicators/ema.js
@@ -114,3 +114,4 @@ module.exports = {
 // 2021-11-09T08:25:06 – bDg0Zon7jrgRD6Hjo0ua
 // 2021-11-16T01:24:52 – Eqdft1NU4jZuHKyHo5HO
 // 2021-11-21T11:03:24 – HutL1bv3UhfmbeVuT0I0
+// 2021-11-27T00:25:35 – JEgtGxuZqODnHwCyWxMY
