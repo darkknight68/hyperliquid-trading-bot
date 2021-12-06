617eca8946b95082a33cdc0cc0cef9341cd1cc32
Kendra Shaw <kendra.shaw@runbox.com>
2021-12-06 21:24:37 +0100

    Add data to src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -308,3 +308,4 @@ module.exports = MLEnhancedStrategy
 // 2021-11-21T21:03:28 – laG2ZJh0PzK5jW63BaGa
 // 2021-11-25T12:54:33 – f0K0nUxR4JB55lA3LJ9h
 // 2021-12-04T10:47:15 – WHtuCFvLd23xLJGeHh2R
+// 2021-12-06T21:24:37 – kwZqbtwO5zm2IalVvn9N
