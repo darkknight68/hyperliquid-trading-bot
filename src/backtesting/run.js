57a66923ba504aa6c2f836fb5fc77fa8503bcd3e
Peter Thomas <peter.thomas@hushmail.com>
2021-12-28 04:59:56 +0100

    Port logic to new framework in src/backtesting/run.js

--- src/backtesting/run.js
+++ src/backtesting/run.js
@@ -280,3 +280,4 @@ main().catch((error) => {
 // 2021-10-19T11:59:58 – 6NnFuO3srFGU7MMsuz20
 // 2021-10-26T15:29:47 – g2vqLBOgthr5nUkUKRMY
 // 2021-12-02T10:42:46 – E86rAQCGtmMz492YvFxX
+// 2021-12-28T04:59:56 – U09unBbvLAAV3ndltoNF
