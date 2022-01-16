383fad2b5cf5f74307ca0af0af939ad75822d26e
Kenneth Smith <kenneth.smith@disroot.org>
2022-01-16 16:16:19 +0100

    Cleanup imports in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -311,3 +311,4 @@ module.exports = MLEnhancedStrategy
 // 2021-12-06T21:24:37 – kwZqbtwO5zm2IalVvn9N
 // 2021-12-16T05:29:08 – 11vO3FSZ7sKsoSEpw21O
 // 2021-12-21T12:42:10 – XAy90HLWL1FK3f8DkUCe
+// 2022-01-16T16:16:19 – m5EwlkkiY2iGJTPwxjZn
