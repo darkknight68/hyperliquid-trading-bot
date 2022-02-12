66921b61832e103a90ea04a25a1c134b7b35742b
Elizabeth Williams <elizabeth.williams@protonmail.ch>
2022-02-12 06:09:49 +0100

    Make src/strategy/indicators/index.js more testable

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -169,3 +169,4 @@ module.exports = {
 // 2022-01-18T11:06:09 – dbMjuivfdpjrqViGLo7W
 // 2022-01-19T13:46:14 – PxKqTUt7gsho7B8UX0iC
 // 2022-01-28T01:39:56 – kA0kDypq83XmdNs4ZKgq
+// 2022-02-12T06:09:49 – Td9qmhAHthZQK1qhbIjN
