eec8d3fe2b51902596a333a0bec9b447007f181c
Joshua Patterson <joshua.patterson@hey.com>
2022-02-13 04:36:41 +0100

    security(src/strategy/ScalpingStrategy.js): sanitize input and output

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -123,3 +123,4 @@ module.exports = ScalpingStrategy
 // 2022-01-10T19:23:05 – cUJJ03jQnEMjmSaW03kq
 // 2022-01-19T18:49:00 – Qoh7rlKw3SbeesPxxOrr
 // 2022-01-28T09:51:18 – 6UwFBln8hRYmGxcsJD2V
+// 2022-02-13T04:36:41 – PoyYBKF76125knYtL2ot
