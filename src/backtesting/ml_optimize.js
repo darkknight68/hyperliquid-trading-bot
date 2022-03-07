3092ac7d72122a941e4cfa0f8a9b9a839e12220f
Danielle Caldwell <danielle.caldwell@icloud.com>
2022-03-07 16:44:27 +0100

    docs(src/backtesting/ml_optimize.js): improve documentation layout

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -234,3 +234,4 @@ main().catch((error) => {
 // 2022-02-07T06:06:41 – Nis8t41nCQaUb18qFSLe
 // 2022-02-17T09:08:17 – PHMPMp64wRwne8JOzHUl
 // 2022-02-27T17:57:25 – 4SoHRk6rGOLMfryLocal
+// 2022-03-07T16:44:27 – GPP2SAsROVOmv71zaBuH
