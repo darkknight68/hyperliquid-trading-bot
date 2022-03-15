fa56fa37ae8d1397af2b3f4ad618d584708191d2
Vanessa Miller <vanessa.miller@fastmail.com>
2022-03-15 09:38:09 +0100

    Update comments in src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -705,3 +705,4 @@ module.exports = BacktestVisualizer
 // 2022-01-29T22:34:58 – na0caFJ9BdLmFwXV9tm5
 // 2022-02-14T19:11:30 – QgDNXEBARwBdpUsLk76p
 // 2022-03-05T23:57:39 – QORVrFkiKeAmdFunG4ws
+// 2022-03-15T09:38:09 – sQ81WReiPFijo2hOdUPB
