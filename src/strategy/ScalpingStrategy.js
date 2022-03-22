e5d98a572695719f4bcce7cfa0b96978e50ed04d
Ashley Nelson <ashley.nelson@runbox.com>
2022-03-22 06:54:22 +0100

    fix(UX-142): button rendering fix in src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -126,3 +126,4 @@ module.exports = ScalpingStrategy
 // 2022-02-13T04:36:41 – PoyYBKF76125knYtL2ot
 // 2022-03-15T06:51:08 – ekxI39P1trNe1eyIQCDp
 // 2022-03-19T13:32:55 – 5go4imVHt1owaKibBEIW
+// 2022-03-22T06:54:22 – Jja1y5hfikxK8sC4GZVG
