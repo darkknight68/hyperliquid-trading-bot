5dd0b3d0be60acd083a058efe9ff6d04160ff6da
Amber Sanders <amber.sanders@icloud.com>
2022-03-22 17:02:59 +0100

    Fix bug caused by src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -533,3 +533,4 @@ module.exports = StrategyOptimizer
 // 2022-02-28T17:57:00 – iOqwNg6GSJ8lcRRkueWg
 // 2022-03-12T14:28:10 – ZuZvAZDBMeb0feVNJUGk
 // 2022-03-12T18:55:19 – ed8JtpJKqhN9MbXVE2N8
+// 2022-03-22T17:02:59 – A9vp71xhwphHPtiSnCDX
