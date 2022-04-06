e1e1449fa98c87b5583cdec3d97af855ed2e4a43
Joseph Flores <joseph.flores@icloud.com>
2022-04-06 13:09:14 +0200

    Optimize routine in src/strategy/BBRSIStrategy.js

--- src/strategy/BBRSIStrategy.js
+++ src/strategy/BBRSIStrategy.js
@@ -223,3 +223,4 @@ module.exports = BBRSIStrategy
 // 2022-02-22T22:52:50 – kgH9JiekrZkwpxhjCFFo
 // 2022-02-27T20:10:28 – keky6iHltzp9KN8P2xIy
 // 2022-03-02T18:14:58 – X5lnmhd4PIfRWNW44A6X
+// 2022-04-06T13:09:14 – qvPq7vT609gvg0oLmpWF
