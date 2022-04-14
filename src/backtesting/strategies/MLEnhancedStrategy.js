05f9bd31b3c9f70405e8970bc6e43d4a65babf43
Brent Estrada <brent.estrada@hushmail.com>
2022-04-14 17:00:51 +0200

    Improve performance in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -317,3 +317,4 @@ module.exports = MLEnhancedStrategy
 // 2022-02-28T04:05:58 – sz6RoXplZrzWse8sBE7z
 // 2022-04-03T18:28:09 – H6CvUcb9eeRukhdYzp93
 // 2022-04-09T05:34:50 – TqYoH9d7WuyO7oH2FMFY
+// 2022-04-14T17:00:51 – 9YWUwrKKEg7Lx2BLSJGu
