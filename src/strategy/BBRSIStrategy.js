e0f2b928a4cd6a9970c5dac2302ce9c133bea947
Erik Robles <erik.robles@protonmail.ch>
2022-05-02 10:34:56 +0200

    Split responsibilities in src/strategy/BBRSIStrategy.js

--- src/strategy/BBRSIStrategy.js
+++ src/strategy/BBRSIStrategy.js
@@ -227,3 +227,4 @@ module.exports = BBRSIStrategy
 // 2022-04-07T23:26:21 – ny9Foa12fv3Y1XreaIyh
 // 2022-04-13T11:16:03 – ZBGLamFLVomzwu74kYsn
 // 2022-04-16T13:17:47 – kc6l70YwsiIePbKMmqTp
+// 2022-05-02T10:34:56 – PGssJtrrpPn2b6RP4AVl
