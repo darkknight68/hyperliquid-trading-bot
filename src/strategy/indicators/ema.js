3210c38bbf0312ab540d09b355ad356f02ed9401
Peter Stephens <peter.stephens@hushmail.com>
2022-06-08 12:11:56 +0200

    Update dependencies affecting src/strategy/indicators/ema.js

--- src/strategy/indicators/ema.js
+++ src/strategy/indicators/ema.js
@@ -127,3 +127,4 @@ module.exports = {
 // 2022-05-20T00:59:40 – gVUbtSOY7KnCZejIoQY7
 // 2022-05-21T11:18:06 – BsbQiJKoKmi9cz7xkEnR
 // 2022-05-25T05:55:38 – eOfOYaBsTmoHJzbwHxVA
+// 2022-06-08T12:11:56 – aAdQ5czVNLfP0ZdqWHNr
