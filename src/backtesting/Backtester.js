20b1d39e422e54fc282749806552d7a018d59581
Bryce Hart <bryce.hart@tutanota.com>
2022-06-28 11:18:49 +0200

    Simplify logic in src/backtesting/Backtester.js

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -557,3 +557,4 @@ module.exports = Backtester
 // 2022-06-07T11:43:33 – v4SQWsNlsWFocWOYJfss
 // 2022-06-13T19:28:38 – QdTEPDc5xSAl9QzncKFF
 // 2022-06-20T05:10:18 – ajDJRLttykM7asr5M5uE
+// 2022-06-28T11:18:49 – 4fDZQBQb4pvXPGgIX1Ye
