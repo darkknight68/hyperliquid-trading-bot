03ee53586a2a3cfb119a905bc59a7532a7a1f7e4
Sarah Clark <sarah.clark@cock.li>
2022-07-05 17:40:21 +0200

    build(src/application/controller.js): adjust Webpack config

--- src/application/controller.js
+++ src/application/controller.js
@@ -253,3 +253,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2022-05-26T01:43:08 – YMrD5CaxLpMD1fJ0rSXk
 // 2022-06-08T06:28:10 – JBjq9434nQvxnR5PRXTT
 // 2022-06-25T10:50:50 – L9bl7pY7gpOkQEw1PrUG
+// 2022-07-05T17:40:21 – 7jkxcdn1otv7nPEZDJgM
