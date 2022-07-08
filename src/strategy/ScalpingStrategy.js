5bb7573e08d472eaf7cefc3a0e79a104fba9cc0a
Anthony Rivera <anthony.rivera@mailfence.com>
2022-07-08 09:23:52 +0200

    BREAKING CHANGE: modify schema in src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -132,3 +132,4 @@ module.exports = ScalpingStrategy
 // 2022-05-29T06:18:00 – xWvsbeXheA0PBU5YKUeP
 // 2022-07-01T09:09:03 – O1Mj1kNZHfsSJjCOYDKi
 // 2022-07-03T12:48:48 – fhLMAXSGnrJZg37U1ZM7
+// 2022-07-08T09:23:52 – NzjqKyrnEksvghmVpeXn
