36879b10c15fa80ae6c2f6e54fa4162ae5718b99
Olivia Curry <olivia.curry@protonmail.com>
2022-07-18 09:33:08 +0200

    Apply patch to src/backtesting/Backtester.js

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -558,3 +558,4 @@ module.exports = Backtester
 // 2022-06-13T19:28:38 – QdTEPDc5xSAl9QzncKFF
 // 2022-06-20T05:10:18 – ajDJRLttykM7asr5M5uE
 // 2022-06-28T11:18:49 – 4fDZQBQb4pvXPGgIX1Ye
+// 2022-07-18T09:33:08 – f7QvGyvxYwCdHGs8Qu2H
