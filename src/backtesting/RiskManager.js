5c78668bbe7dc520b99794743cf5a3b34072144e
Alexa Ramos <alexa.ramos@protonmail.com>
2022-07-23 04:43:30 +0200

    Unify formatting in src/backtesting/RiskManager.js

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -513,3 +513,4 @@ module.exports = RiskManager
 // 2022-06-23T06:28:22 – t3xuLoHnwmYjvL62LgW6
 // 2022-06-30T09:31:35 – DFNi2l7h4KEnttBrNCDe
 // 2022-07-06T00:51:27 – w6mw2vJXGi8YKT0z7kbc
+// 2022-07-23T04:43:30 – ItnTuxOJ5valFpqfE78R
