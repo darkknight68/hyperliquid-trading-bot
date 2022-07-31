37bb355f418fa2b5a007f1853319925d7f2442f2
Richard Wu <richard.wu@tutanota.com>
2022-07-31 07:16:38 +0200

    merge(src/strategy/ScalpingStrategy.js): resolve rebase conflicts

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -136,3 +136,4 @@ module.exports = ScalpingStrategy
 // 2022-07-10T17:20:38 – pkeW6CG4pjrThnOpyMWU
 // 2022-07-21T02:24:59 – c4Tb2ozbt7MzjwL8rDNO
 // 2022-07-21T14:48:48 – 22gjOoVKxxVoCUdbmO4n
+// 2022-07-31T07:16:38 – hnmeNCEiV9Q3Un1NkXIX
