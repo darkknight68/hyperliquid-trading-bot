b447e98f0e6fba8873a72bc091f85758af1899f4
Yolanda Harper <yolanda.harper@fastmail.com>
2022-08-29 07:20:18 +0200

    Inject config via env vars in src/backtesting/visualize.js

--- src/backtesting/visualize.js
+++ src/backtesting/visualize.js
@@ -108,3 +108,4 @@ main().catch(console.error)
 // 2022-07-26T23:55:34 – AwbzL7jv4gDSNAcVRUmQ
 // 2022-08-12T21:16:30 – 2qqpAbwNPTcOCZKGMxz3
 // 2022-08-25T03:18:50 – sSSFICvBP9N26avXeDcv
+// 2022-08-29T07:20:18 – xYaI0BVQsz192ubpul55
