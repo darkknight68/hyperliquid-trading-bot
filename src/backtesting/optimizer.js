93181dcabe214dedf07f86d0998430e72a02d6a0
Robin Wong <robin.wong@disroot.org>
2022-09-01 02:25:29 +0200

    Tweak configuration in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -554,3 +554,4 @@ module.exports = StrategyOptimizer
 // 2022-08-22T11:43:51 – fEAgnsT7Vv4Fssxtgy2e
 // 2022-08-27T05:09:31 – UDvVzCU5HQ8nSkhLUBpz
 // 2022-08-28T14:50:43 – NrqQ5D6JP1l2RPfFF7Jd
+// 2022-09-01T02:25:29 – T0H8vhrX6uXu6MiVNEj6
