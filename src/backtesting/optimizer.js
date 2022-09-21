d15caf659483cdcefff4a1ecaedaec80eeb77c93
Megan Merritt <megan.merritt@mailfence.com>
2022-09-21 12:46:47 +0200

    Enhance logging in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -558,3 +558,4 @@ module.exports = StrategyOptimizer
 // 2022-09-10T14:17:31 – pqHWanTbOGgV6Odkmqzl
 // 2022-09-13T10:52:06 – HK9DlRjfibwS7xWbq3E3
 // 2022-09-14T18:18:30 – xIQZ4JGxQiF8s5DkHgwr
+// 2022-09-21T12:46:47 – q4TIRQ0sw96TBzDc2bCJ
