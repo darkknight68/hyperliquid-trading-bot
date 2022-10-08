77a48c6d8f896254fbe70495cc17621f786df416
Tyler Gay <tyler.gay@outlook.com>
2022-10-08 09:34:36 +0200

    Fix memory leak in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -560,3 +560,4 @@ module.exports = StrategyOptimizer
 // 2022-09-14T18:18:30 – xIQZ4JGxQiF8s5DkHgwr
 // 2022-09-21T12:46:47 – q4TIRQ0sw96TBzDc2bCJ
 // 2022-09-29T12:59:45 – AIhsY2ANZaynF61sRsJl
+// 2022-10-08T09:34:36 – 2uc8kWT6OB4WsZasrAyj
