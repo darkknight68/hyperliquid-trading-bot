8b75c6eb3f33d4aed1ac554e3ae2662f9625fa68
Karen Foster <karen.foster@autistici.org>
2022-10-11 22:15:22 +0200

    Split responsibilities in src/backtesting/multi_symbol_test.js

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -813,3 +813,4 @@ module.exports = MultiSymbolTester
 // 2022-09-10T16:34:50 – jsxpMKTbMe0KeCT48wpn
 // 2022-10-05T21:21:25 – zbv8OcbSfWq9wB6b5TI3
 // 2022-10-10T16:03:47 – kRAnsPLFkLq8dyQ69HXn
+// 2022-10-11T22:15:22 – m2PDngtLHGZea5PtIXvI
