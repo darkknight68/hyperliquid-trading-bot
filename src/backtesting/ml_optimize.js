4f02c7674dfc098e0d501e848689145ca462cf95
Teresa Walker <teresa.walker@duck.com>
2022-10-24 14:46:48 +0200

    feat(core): integrate new API into src/backtesting/ml_optimize.js

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -256,3 +256,4 @@ main().catch((error) => {
 // 2022-09-24T11:25:48 – 0RA6TjU8NWmoeccafKFO
 // 2022-10-10T18:07:11 – qt3ILlcssZsDwWTAnaM9
 // 2022-10-14T02:03:49 – 0Ux8Kpw4xZzxqyp1fvJe
+// 2022-10-24T14:46:48 – lP4TZgMmKelaNH1Abhv8
