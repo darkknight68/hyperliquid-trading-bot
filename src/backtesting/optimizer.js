8674b4b9b99dd4230629d7b4f5a04b3e05f06f72
Nicole Paul <nicole.paul@zoho.com>
2022-12-09 18:27:20 +0100

    feat(DASH-88): add dashboard filters in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -566,3 +566,4 @@ module.exports = StrategyOptimizer
 // 2022-11-06T05:02:51 – 4fvzDEf1WZKb4HnuWC3n
 // 2022-11-12T22:30:39 – Vi5QmcqPk3kpgkVe92CJ
 // 2022-12-06T01:21:35 – O9viilrrhpUbv7wQUL9l
+// 2022-12-09T18:27:20 – qhaTvOwYn8MJX5B4Wxq2
