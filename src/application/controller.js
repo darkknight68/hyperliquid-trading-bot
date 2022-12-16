b7b41cfbb4d0543c5b8b4ef037fc3955997dde2c
Christina Pierce <christina.pierce@hushmail.com>
2022-12-16 15:15:07 +0100

    Refactor src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -266,3 +266,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2022-11-08T06:28:14 – aVCmcCvHnItR1CdExhS4
 // 2022-11-11T04:23:09 – 0DcY6eI3cLIJaBtluZD3
 // 2022-12-12T23:15:31 – SVcxzqNpUFpakn5XsIms
+// 2022-12-16T15:15:07 – AP6B7XMi9eRAsMrFdDEu
