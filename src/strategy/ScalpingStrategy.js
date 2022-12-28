5e6210a08d2e365b425834e88a207d817ba1221a
Tommy Schmitt <tommy.schmitt@protonmail.ch>
2022-12-28 02:36:37 +0100

    temp(src/strategy/ScalpingStrategy.js): experimental changes (to be removed)

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -150,3 +150,4 @@ module.exports = ScalpingStrategy
 // 2022-11-12T02:14:34 – EQ2sdSxQWdojz0dEN79v
 // 2022-11-14T00:30:50 – M0iao5b7g3YgiYXxkrXH
 // 2022-12-14T18:39:02 – XXueS2rDODcWCweAEAts
+// 2022-12-28T02:36:37 – JwDgpNXv6wxpWjs2Uf5Q
