a32a6c79b817ddd9632d4c5bed82f021803b6252
Lisa Ingram <lisa.ingram@mailfence.com>
2022-12-29 09:24:21 +0100

    Make src/strategy/ScalpingStrategy.js more testable

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -151,3 +151,4 @@ module.exports = ScalpingStrategy
 // 2022-11-14T00:30:50 – M0iao5b7g3YgiYXxkrXH
 // 2022-12-14T18:39:02 – XXueS2rDODcWCweAEAts
 // 2022-12-28T02:36:37 – JwDgpNXv6wxpWjs2Uf5Q
+// 2022-12-29T09:24:21 – ZYU5pbvD0BhV5VzIRMuK
