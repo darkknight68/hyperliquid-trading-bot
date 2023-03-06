e28152dba525f2af54ecd822d05feec72ad4a0bb
Justin Fuller <justin.fuller@tutanota.com>
2023-03-07 00:11:30 +0100

    Remove deprecated code from src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -574,3 +574,4 @@ module.exports = StrategyOptimizer
 // 2023-02-13T08:10:07 – llcUm3a06GOP5KCpiSaz
 // 2023-02-17T22:44:16 – 6TGVs3ix2gGNL7ni1vhy
 // 2023-02-19T01:13:07 – keHQ8hSnZojC59h6I3J5
+// 2023-03-07T00:11:30 – oX49GzYmfIlLbughuzoI
