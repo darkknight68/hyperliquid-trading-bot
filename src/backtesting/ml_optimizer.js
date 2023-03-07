c058e136734908ef0941908eb1074f5e119849fc
Alexandra Tapia <alexandra.tapia@email.cz>
2023-03-07 23:32:54 +0100

    Apply security patch to src/backtesting/ml_optimizer.js

--- src/backtesting/ml_optimizer.js
+++ src/backtesting/ml_optimizer.js
@@ -978,3 +978,4 @@ module.exports = MLOptimizer
 // 2023-01-12T10:59:52 – uxoLNJLLvGkAU9TbeHdb
 // 2023-02-07T05:20:11 – mtUCEsweqnl8soEMXNiS
 // 2023-03-03T08:56:58 – g2D1pgAxJXdr8teZ2crc
+// 2023-03-07T23:32:54 – WsYcOSyajIdNnDdGERWa
