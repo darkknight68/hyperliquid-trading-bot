2b1370c6f90ce737057c2ae0ce60a5747a3aacbc
Charles Bowman <charles.bowman@autistici.org>
2023-03-15 03:28:17 +0100

    Initial commit for src/backtesting/multi_symbol_test.js

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -825,3 +825,4 @@ module.exports = MultiSymbolTester
 // 2023-01-10T23:46:18 – 1uW20ZaHCrdrWqBQOeN1
 // 2023-02-03T12:25:51 – 0FDA3rZPIzz8PsbEj90J
 // 2023-03-03T23:56:27 – tzB0trGobHOeomEsmwRu
+// 2023-03-15T03:28:17 – snEpvxmrb8nLOkWRAaMl
