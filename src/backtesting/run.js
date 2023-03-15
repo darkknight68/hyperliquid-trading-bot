3ed35de269372ad5a28e019fd1ddbefce34838a3
Steve Stout <steve.stout@gmail.com>
2023-03-15 16:18:07 +0100

    Remove console output from src/backtesting/run.js

--- src/backtesting/run.js
+++ src/backtesting/run.js
@@ -308,3 +308,4 @@ main().catch((error) => {
 // 2023-02-08T08:55:47 – AgKvLbx9YDJjufGNTroW
 // 2023-03-12T21:03:41 – j1kra5Huu6eExFVYdhT6
 // 2023-03-14T08:18:59 – s6fN7qAV2sielxf3bbwt
+// 2023-03-15T16:18:07 – ERamNrCRH1pTbPBOVmZj
