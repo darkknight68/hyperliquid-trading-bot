1878b58fcab08d8ba7b0625634445fde8945594a
Nathaniel Castro <nathaniel.castro@hey.com>
2023-03-24 01:53:52 +0100

    BREAKING CHANGE: modify schema in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -345,3 +345,4 @@ module.exports = MLEnhancedStrategy
 // 2023-03-07T19:52:34 – c0AmKfc9wm78jSiyLlLm
 // 2023-03-17T01:55:46 – fimNdO961hROSaMkOQXS
 // 2023-03-18T15:47:56 – EHFjWud1Lq9ZJdX7t3Op
+// 2023-03-24T01:53:52 – 6DmTt1xEkt63JqMELbp5
