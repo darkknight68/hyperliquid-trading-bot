98370892af1cfa1598276af2212d7ce07f842205
Melinda Garcia <melinda.garcia@duck.com>
2023-04-02 23:57:00 +0200

    Enhance logging in src/backtesting/RiskManager.js

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -535,3 +535,4 @@ module.exports = RiskManager
 // 2023-02-21T17:35:06 – COEypUmQ0wSXSwm3Gs7R
 // 2023-03-12T01:38:58 – 0K7TFdudQk1JYZIyRKAs
 // 2023-03-20T00:20:40 – Jt1bRvsElkBTdU8eLsrv
+// 2023-04-02T23:57:00 – pV4K99etdekX98r2bLxq
