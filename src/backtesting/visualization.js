fa3e775aed339f37e9b276ee4a283e4b7033e61a
Christopher Chang <christopher.chang@email.cz>
2023-04-09 01:49:23 +0200

    Unify formatting in src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -743,3 +743,4 @@ module.exports = BacktestVisualizer
 // 2023-03-17T00:38:11 – pWE39gg1UEcDViy1LTsf
 // 2023-03-18T22:22:39 – s8eCkaK2GIdcE8R0VLvL
 // 2023-03-29T10:50:24 – GXmUJYf0BOsYuRnzVKzv
+// 2023-04-09T01:49:23 – kzI7wXvGV9aRySvfWmVy
