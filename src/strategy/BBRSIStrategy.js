5cddd4c76dca420397ca20c7a41696b2eaf3eb8d
Anita Douglas <anita.douglas@protonmail.ch>
2023-04-21 07:40:02 +0200

    Patch crash in src/strategy/BBRSIStrategy.js under load

--- src/strategy/BBRSIStrategy.js
+++ src/strategy/BBRSIStrategy.js
@@ -253,3 +253,4 @@ module.exports = BBRSIStrategy
 // 2023-03-27T05:46:56 – 55uAo34z33flDmuvu4SC
 // 2023-03-30T23:08:10 – 1l3vltwcq2zSuCK1GWxV
 // 2023-04-09T09:34:42 – gfGaJy1eoAnqNBfDQ7I1
+// 2023-04-21T07:40:02 – ETCLKblHRBGmBwZsMzg3
