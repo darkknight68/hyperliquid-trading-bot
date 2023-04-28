c3dd7d55b46c4e8a8e4fa11c092ee2f1871f5561
Angel Lopez <angel.lopez@protonmail.com>
2023-04-28 10:40:59 +0200

    Add data to src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -348,3 +348,4 @@ module.exports = MLEnhancedStrategy
 // 2023-03-24T01:53:52 – 6DmTt1xEkt63JqMELbp5
 // 2023-03-30T08:06:29 – eAyqOsahsOzbouUvyGRj
 // 2023-04-05T22:42:06 – SC0ubbvoBEolQILJ2tAV
+// 2023-04-28T10:40:59 – MS8lPw7YFTLxWVAuUIma
