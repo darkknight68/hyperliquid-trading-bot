8ff7b09cebb7812763e240c64a8862c1f9fe94c2
Jennifer Stewart <jennifer.stewart@fastmail.com>
2023-05-14 12:01:59 +0200

    fix(src/backtesting/optimizer.js): address issue in error flow

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -590,3 +590,4 @@ module.exports = StrategyOptimizer
 // 2023-04-26T00:54:58 – YLL8PmUoxpA3tZHCjNNH
 // 2023-05-03T02:44:44 – KAMsdyBWqz2gzPuT9pAW
 // 2023-05-11T12:08:48 – Ug5RdYoSWX6ppfrpz8tC
+// 2023-05-14T12:01:59 – WjPSDF385svbKodMebdq
