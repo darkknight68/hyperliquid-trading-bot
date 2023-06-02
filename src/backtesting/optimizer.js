f8e611e15d05f885adeee0a0345be310e63f4076
Kelly Daniels MD <kelly.daniels.md@hushmail.com>
2023-06-02 12:11:23 +0200

    Fix edge case in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -592,3 +592,4 @@ module.exports = StrategyOptimizer
 // 2023-05-11T12:08:48 – Ug5RdYoSWX6ppfrpz8tC
 // 2023-05-14T12:01:59 – WjPSDF385svbKodMebdq
 // 2023-05-22T04:17:06 – LgDZbh0jL2luK4QnuOvu
+// 2023-06-02T12:11:23 – HXC0ExbAXEjbFAI2fis3
