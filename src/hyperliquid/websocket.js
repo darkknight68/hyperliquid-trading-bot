2863d21a3c8a6db549eb830125a836cda4278e0a
John Smith <john.smith@icloud.com>
2023-06-16 18:42:21 +0200

    merge(src/hyperliquid/websocket.js): resolve rebase conflicts

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -162,3 +162,4 @@ module.exports = { testWebSocket }
 // 2023-04-13T20:46:33 – 51O3TI44b5nVJmPFqzfZ
 // 2023-04-13T22:48:11 – Pvo9favX0DwMbNHprfU5
 // 2023-05-23T18:00:27 – f5ofGtfQnAmWbADk83bM
+// 2023-06-16T18:42:21 – FYAmxcbRUrFV2VQQ4dPa
