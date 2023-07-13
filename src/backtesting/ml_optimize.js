cfea28ae2ca9e7f2b1239a871c1fc93910fa9013
Christopher Walker <christopher.walker@email.cz>
2023-07-13 22:30:10 +0200

    Apply patch to src/backtesting/ml_optimize.js

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -272,3 +272,4 @@ main().catch((error) => {
 // 2023-05-28T13:56:18 – SUMsd9fzF0k9J7n2EsDI
 // 2023-06-12T12:18:06 – 4HtfkHjqH5KAG8SnrbjO
 // 2023-07-04T17:24:42 – pPjjYZoGL7GINg1rvewI
+// 2023-07-13T22:30:10 – HE65MhhDkzlAHUiAE5gU
