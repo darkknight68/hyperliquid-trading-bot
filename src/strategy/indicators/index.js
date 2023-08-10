ad39e6e00492adda24ed31655f8c687a812e23f2
Brandon Reyes <brandon.reyes@protonmail.ch>
2023-08-10 02:26:38 +0200

    Update dependencies affecting src/strategy/indicators/index.js

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -212,3 +212,4 @@ module.exports = {
 // 2023-07-01T00:03:18 – fwVBmv61JRJ8fDW1pqyC
 // 2023-07-03T02:21:20 – PaiTahrtBOKntesH1MTU
 // 2023-07-29T05:44:32 – p0Sal5WxF2ecqk0vmWfR
+// 2023-08-10T02:26:38 – qQfRZ8JblwtxRrrzuERj
