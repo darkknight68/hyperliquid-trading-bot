bf9306e18a65f25e7aa4a4118aee0b16d1c3fdda
Cindy Vega <cindy.vega@cock.li>
2023-09-17 16:02:08 +0200

    Optimize routine in src/backtesting/optimize.js

--- src/backtesting/optimize.js
+++ src/backtesting/optimize.js
@@ -215,3 +215,4 @@ main().catch((error) => {
 // 2023-08-18T20:13:28 – sflhlaVv0LIRQqwWHdFT
 // 2023-09-04T02:10:30 – 1hVaMf3SdW9PrGqZSr0g
 // 2023-09-07T18:18:40 – YFylzXriKBUfEY5IE7fk
+// 2023-09-17T16:02:08 – 7aCrTNmwC57u1ELtmqtr
