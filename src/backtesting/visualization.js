78fcc9fca9a3327199b4fab89b4eb86d07708626
Cory Coleman <cory.coleman@duck.com>
2023-10-09 13:37:59 +0200

    hotfix(src/backtesting/visualization.js): emergency fix for prod bug

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -765,3 +765,4 @@ module.exports = BacktestVisualizer
 // 2023-09-30T21:59:45 – DajJ01TDoNlRldfHOPlk
 // 2023-10-01T05:36:31 – 2ayil1xOXN2NvcdAUj9L
 // 2023-10-05T04:57:13 – sZFIqzrCya59d7OiuBQc
+// 2023-10-09T13:37:59 – 0Dcdn8c0EFJeAztTHb2n
