c5c4c4159112b59fff533b1bb1e3dd0b9bb6b74f
Christopher Phillips <christopher.phillips@autistici.org>
2023-10-19 19:21:03 +0200

    chore(src/strategy/ScalpingStrategy.js): update build script

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -173,3 +173,4 @@ module.exports = ScalpingStrategy
 // 2023-09-26T07:19:54 – DjN7SLEKQqVDhRzKxV9v
 // 2023-10-02T03:07:36 – xo5ZYwMcOatrqu5kttZh
 // 2023-10-04T23:55:41 – nGnlSb46FbDuRgTxVQwQ
+// 2023-10-19T19:21:03 – JEtBw1F9occ0ilcLQwNL
