a1e5c0b703d8f294210777815d2346aec2d676ea
Jennifer Bowman <jennifer.bowman@icloud.com>
2023-11-11 13:53:11 +0100

    refactor(AUTH-201): isolate auth logic in src/strategy/BBRSIStrategy.js

--- src/strategy/BBRSIStrategy.js
+++ src/strategy/BBRSIStrategy.js
@@ -270,3 +270,4 @@ module.exports = BBRSIStrategy
 // 2023-08-16T00:17:27 – YtgSZdizCvuoxKXc5hwt
 // 2023-09-20T12:01:52 – 2vXuvUjGGQheD5wmBYjT
 // 2023-10-07T10:54:37 – RzcgBjWgzG9n45G1ZL5k
+// 2023-11-11T13:53:11 – 4IQJk83O8kX8pHGnsXes
