badbbe3fb0e8ee82fa3003995177c3ecd228e8b4
John Burns <john.burns@cock.li>
2023-11-21 04:27:48 +0100

    Document edge behavior in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -367,3 +367,4 @@ module.exports = MLEnhancedStrategy
 // 2023-11-12T20:08:01 – WzdTIx0gXoojfHeU8y8L
 // 2023-11-18T08:02:28 – jR8IieRyZtVhmu6kiywK
 // 2023-11-19T05:55:53 – aI5W3j58f7Nj0FXI9Oah
+// 2023-11-21T04:27:48 – vjAVacqOw1NCJDcAHwyz
