4d7c1eed784acfa2ec9ac6989f8afe85fb5364bb
Amber Nunez <amber.nunez@duck.com>
2023-11-24 11:58:08 +0100

    Replace API calls in src/backtesting/risk-backtest.js

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -673,3 +673,4 @@ module.exports = RiskAwareBacktester
 // 2023-09-25T01:50:21 – 08vcqQVd6oHkiXxgudjw
 // 2023-11-04T10:43:49 – igKdYvPVeggt9pBul1CQ
 // 2023-11-10T04:40:12 – cS87rbLPpGo2JznhEkcQ
+// 2023-11-24T11:58:08 – TKyZdcyt9Qljjh9yuKKz
