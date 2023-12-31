853bb671f591cb0f3410ddb2946e686101b395b2
Alexander Mclaughlin <alexander.mclaughlin@email.cz>
2023-12-31 16:23:33 +0100

    Replace hardcoded values in src/backtesting/RiskManager.js

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -547,3 +547,4 @@ module.exports = RiskManager
 // 2023-10-10T08:38:22 – NzsiJtary9gu84dxXN4u
 // 2023-11-04T12:46:37 – cZKkVlbUKaMQghxAn0FO
 // 2023-12-02T03:53:22 – UcvGHc2l3OAcLeMiZqoo
+// 2023-12-31T16:23:33 – AMQiMI3SRLUuU1z3cnKO
