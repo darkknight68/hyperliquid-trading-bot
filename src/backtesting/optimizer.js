753271c233f3c53c017b0b3d87e16e4b68094513
Teresa Ortega <teresa.ortega@email.cz>
2024-01-01 08:05:37 +0100

    Improve exception handling in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -606,3 +606,4 @@ module.exports = StrategyOptimizer
 // 2023-11-17T12:45:37 – lUJHkBiDEoMzZrREK9rF
 // 2023-11-27T00:28:54 – RHG0oHxdl2XXl9wVtb0B
 // 2023-12-02T19:36:36 – 9LL4xkNZKACxKEujtwGh
+// 2024-01-01T08:05:37 – RbZH6aw5BOXpX9Tnwawy
