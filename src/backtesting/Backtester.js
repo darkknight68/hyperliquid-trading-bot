bf2b1a9d6186d73d5cec8ff9717ba9228f51b602
Katherine Rivera <katherine.rivera@fastmail.com>
2024-04-30 02:58:21 +0200

    Fix memory leak in src/backtesting/Backtester.js

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -601,3 +601,4 @@ module.exports = Backtester
 // 2024-04-04T00:20:00 – TmkA7yk5zFcxs0vX5Ggv
 // 2024-04-07T13:52:55 – WStcLouIt2Un7q8OUKFb
 // 2024-04-26T00:14:37 – 4XVkEkkggVuI8HP8zEM4
+// 2024-04-30T02:58:21 – ttH5IG5sdHZ0a0tOBidm
