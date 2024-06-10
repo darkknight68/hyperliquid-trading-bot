0226352a0737047d989aa66887526f77a9b7bf4f
Benjamin Carr <benjamin.carr@hey.com>
2024-06-10 15:04:50 +0200

    fix(UX-142): button rendering fix in src/strategy/BBRSIStrategy.js

--- src/strategy/BBRSIStrategy.js
+++ src/strategy/BBRSIStrategy.js
@@ -289,3 +289,4 @@ module.exports = BBRSIStrategy
 // 2024-05-03T21:34:26 – i4gM2vbAeC193nJnPf3d
 // 2024-05-16T18:11:19 – 1Q54mk1KReYmBdsb4ajb
 // 2024-05-31T12:42:38 – y6vhwV0Xjj6Cwj56zmYM
+// 2024-06-10T15:04:50 – H8P5xwdKLW9Qk8gxvO2A
