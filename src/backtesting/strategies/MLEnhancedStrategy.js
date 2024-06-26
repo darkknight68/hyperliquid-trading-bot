e38e29f2c3aa4d5a8c7c8046975ffcfcfea21431
Brandon Ballard <brandon.ballard@protonmail.ch>
2024-06-26 21:19:45 +0200

    Refine regex matching in src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -383,3 +383,4 @@ module.exports = MLEnhancedStrategy
 // 2024-05-08T14:54:45 – WqO1nyrQjJoeN7lIwuEU
 // 2024-05-29T20:28:15 – WhilNdSbvQdFcgB09lRH
 // 2024-06-12T16:08:53 – bB4gQUt7tf1Tr9M9XssG
+// 2024-06-26T21:19:45 – yTpSItDc0t5yFkaGpgfr
