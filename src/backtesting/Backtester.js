225efde414d3c0ac84c3d37a8c24df88553723e2
Edward Turner <edward.turner@tutanota.com>
2024-07-01 04:09:50 +0200

    Format src/backtesting/Backtester.js according to style guide

--- src/backtesting/Backtester.js
+++ src/backtesting/Backtester.js
@@ -604,3 +604,4 @@ module.exports = Backtester
 // 2024-04-30T02:58:21 – ttH5IG5sdHZ0a0tOBidm
 // 2024-05-16T10:38:09 – 6iQN3zA8tawqywQvMtga
 // 2024-06-25T20:03:25 – XKE2Hu4K4SJp9Li9ZShh
+// 2024-07-01T04:09:50 – AHhHZKWrcIgmuZ2vF2bH
