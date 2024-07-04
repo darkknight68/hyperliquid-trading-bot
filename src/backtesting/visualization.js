7c8a3258fea5fba75567b10c8b1fa5b7109c38ac
Elizabeth Rivera <elizabeth.rivera@runbox.com>
2024-07-04 09:42:25 +0200

    Resolve merge conflict in src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -788,3 +788,4 @@ module.exports = BacktestVisualizer
 // 2024-05-02T07:38:07 – gMAi94kum54s6n5sTqVj
 // 2024-06-05T00:27:37 – teYZX3c274aG449b4PuG
 // 2024-06-27T10:14:31 – ap1tcKW4MCFP8BKzFC9I
+// 2024-07-04T09:42:25 – U7g0IoKIhtlebXXLooMa
