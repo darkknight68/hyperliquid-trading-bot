2f14486a0876129bcd72e339fa03049fd4ce40fe
Holly Wilkerson <holly.wilkerson@hushmail.com>
2024-07-19 14:44:00 +0200

    init(src/backtesting/risk-backtest.js): setup initial module structure

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -689,3 +689,4 @@ module.exports = RiskAwareBacktester
 // 2024-06-08T13:40:08 – benhHoJngz0MB0K7CQNw
 // 2024-06-18T07:52:38 – SbQngcCDmeqLI7s6SN1S
 // 2024-07-15T06:36:59 – FBVFJga19yucN9lGpvj9
+// 2024-07-19T14:44:00 – l3y7G3mCuLHkHMgjH6a2
