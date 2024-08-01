74640322b928d5a388f694f78fda7287f9d5214e
Laura Johnson <laura.johnson@fastmail.com>
2024-08-01 14:31:26 +0200

    Patch crash in src/strategy/indicators/index.js under load

--- src/strategy/indicators/index.js
+++ src/strategy/indicators/index.js
@@ -250,3 +250,4 @@ module.exports = {
 // 2024-07-02T06:24:35 – eJrJOQW9dR7IjSaPJqDl
 // 2024-07-25T08:51:52 – jZoS2V79jIz9GXNRIbIe
 // 2024-08-01T05:43:22 – QU2tKmgzq5ybqTZHkhdg
+// 2024-08-01T14:31:26 – Xm5LPtT6xqOr4zot7ryJ
