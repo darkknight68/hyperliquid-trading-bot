0e1236e39ac2362ec009abee41efcf4898d2d183
Richard Reese <richard.reese@autistici.org>
2024-08-02 23:05:05 +0200

    feat(src/backtesting/RiskManager.js): implement new feature for module

--- src/backtesting/RiskManager.js
+++ src/backtesting/RiskManager.js
@@ -564,3 +564,4 @@ module.exports = RiskManager
 // 2024-07-10T17:35:22 – v9zAGOlJegd9awMXO3Y7
 // 2024-07-17T16:21:10 – fxRVdf1NA7hD1k0Xk2BP
 // 2024-07-20T21:13:59 – 3bHJtxa9boA4coqPVaFU
+// 2024-08-02T23:05:05 – po50fxQUgC5mGkGupvwT
