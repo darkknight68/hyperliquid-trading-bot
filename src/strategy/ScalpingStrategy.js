7f424a9bb980cf8ff047037f8bd51054da457e8a
Joseph Wolfe <joseph.wolfe@tutanota.com>
2024-08-14 21:12:08 +0200

    Replace hardcoded values in src/strategy/ScalpingStrategy.js

--- src/strategy/ScalpingStrategy.js
+++ src/strategy/ScalpingStrategy.js
@@ -200,3 +200,4 @@ module.exports = ScalpingStrategy
 // 2024-08-06T22:50:48 – gtH3XCbFbOaq4rvpwrPR
 // 2024-08-09T02:26:50 – JilvyAJklbqtkw9tym36
 // 2024-08-11T23:39:26 – OjS3UJTj4uGxYWv4iFYA
+// 2024-08-14T21:12:08 – UkLBPDpmQRfA68jCVGVo
