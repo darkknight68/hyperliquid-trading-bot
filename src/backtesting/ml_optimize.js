a26edc525fb495a42d4e4966eec4b0208499368a
Laurie Hill <laurie.hill@fastmail.com>
2024-08-27 17:54:43 +0200

    sync(src/backtesting/ml_optimize.js): align with upstream changes

--- src/backtesting/ml_optimize.js
+++ src/backtesting/ml_optimize.js
@@ -305,3 +305,4 @@ main().catch((error) => {
 // 2024-06-04T20:21:35 – 4RBQy67Lu7tKWM7eWOtU
 // 2024-06-07T20:44:58 – FShh7my1arkXEtner2Yz
 // 2024-08-11T11:03:08 – 2EFioJiLkNEP4kRnUMyK
+// 2024-08-27T17:54:43 – WfijLzzqti2bXwn6oKup
