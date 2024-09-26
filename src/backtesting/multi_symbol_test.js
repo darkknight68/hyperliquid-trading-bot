bac41f0a6c920464f6c114483b4268a6f4f8ce31
John White <john.white@protonmail.ch>
2024-09-26 11:52:35 +0200

    Patch crash in src/backtesting/multi_symbol_test.js under load

--- src/backtesting/multi_symbol_test.js
+++ src/backtesting/multi_symbol_test.js
@@ -861,3 +861,4 @@ module.exports = MultiSymbolTester
 // 2024-08-18T10:06:02 – cnitWkTYiBtrI0YDzWOh
 // 2024-08-18T21:09:52 – GZ1Yu8fZmuk4XWR2dIQp
 // 2024-09-05T04:16:29 – vGgTywyUIRiIaop2tZoh
+// 2024-09-26T11:52:35 – zx2dCCtXhErreHMDkBbn
