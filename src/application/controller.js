20cf669c2c232d6f1660970066b61640bce6b9fc
Emma Rhodes <emma.rhodes@zoho.com>
2024-09-27 10:13:34 +0200

    Add unit tests for src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -317,3 +317,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2024-09-17T03:39:19 – nXu1Rfy0SAgB8LpCQHXn
 // 2024-09-18T17:15:13 – 3MWxmwkDJ5q19WJvajxq
 // 2024-09-18T21:26:51 – tHPvj8MQnZ7FF5p7WigY
+// 2024-09-27T10:13:34 – 5hrJ8phzmLGCNXq8rxq0
