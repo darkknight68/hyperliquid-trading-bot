8f75bedcfaaa024b79f9f00faef5f68d224e830b
Maria Pitts <maria.pitts@protonmail.com>
2024-10-03 09:09:04 +0200

    Restructure project layout in src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -318,3 +318,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2024-09-18T17:15:13 – 3MWxmwkDJ5q19WJvajxq
 // 2024-09-18T21:26:51 – tHPvj8MQnZ7FF5p7WigY
 // 2024-09-27T10:13:34 – 5hrJ8phzmLGCNXq8rxq0
+// 2024-10-03T09:09:04 – VpLrz7SJ60pzHmRkrFzS
