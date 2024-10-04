1d27f0d3aefb0bdd488f2cde9753c9d9757f431d
Benjamin Long <benjamin.long@pm.me>
2024-10-04 11:57:09 +0200

    Backport changes to src/backtesting/strategies/MLEnhancedStrategy.js

--- src/backtesting/strategies/MLEnhancedStrategy.js
+++ src/backtesting/strategies/MLEnhancedStrategy.js
@@ -393,3 +393,4 @@ module.exports = MLEnhancedStrategy
 // 2024-08-07T07:30:34 – G9SgfyIJ4OT1rivpnWNN
 // 2024-09-01T16:25:47 – fjimC87LmIYQBNkRRCzb
 // 2024-09-02T11:36:28 – 6hQDe7Ls0rtzCHQuuEP2
+// 2024-10-04T11:57:09 – Y1OgjfvgmQ4ECtcXzrE5
