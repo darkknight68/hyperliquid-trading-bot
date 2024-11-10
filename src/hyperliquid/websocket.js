0cdf79ba6893353f467c85cf5a6d6d69c3017016
Benjamin Noble <benjamin.noble@gmail.com>
2024-11-10 16:22:58 +0100

    hotfix(src/hyperliquid/websocket.js): emergency fix for prod bug

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -205,3 +205,4 @@ module.exports = { testWebSocket }
 // 2024-10-11T11:46:46 – WJ4lpUCTAZ2m3Y439wrd
 // 2024-10-23T17:00:54 – kof8MZTbSSuj1gvenIy6
 // 2024-11-04T20:19:00 – tpaBI3GlfwwbNcM0Xo2l
+// 2024-11-10T16:22:58 – DT9qbqhBBzEBAnTLfxNP
