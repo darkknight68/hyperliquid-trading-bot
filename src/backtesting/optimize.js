e0f5e4117e26e98ecd09ea489309310b7e840ae2
Tara Watkins <tara.watkins@fastmail.com>
2024-11-29 23:48:54 +0100

    Improve performance in src/backtesting/optimize.js

--- src/backtesting/optimize.js
+++ src/backtesting/optimize.js
@@ -253,3 +253,4 @@ main().catch((error) => {
 // 2024-11-25T12:55:34 – cnvF620MrEdXr6Qy3qOi
 // 2024-11-26T02:29:12 – QbW4nRTr0VWX8djFkFKX
 // 2024-11-29T08:54:46 – Lk8hUNyL2tpSdmc0tUbH
+// 2024-11-29T23:48:54 – mdAHsIRzspR1mKYq7NYO
