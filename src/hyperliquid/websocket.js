c43442d1b1a877762661fb016ec060c11837c7b2
Brian Mata <brian.mata@gmail.com>
2024-12-15 04:50:28 +0100

    Update src/hyperliquid/websocket.js

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -208,3 +208,4 @@ module.exports = { testWebSocket }
 // 2024-11-10T16:22:58 – DT9qbqhBBzEBAnTLfxNP
 // 2024-11-26T20:07:40 – qIDc2W7KAJTlinUFGGL2
 // 2024-12-12T05:10:23 – moAUHx5er4iuWge6xbjp
+// 2024-12-15T04:50:28 – jZaiWWb8rc879OxDfn9l
