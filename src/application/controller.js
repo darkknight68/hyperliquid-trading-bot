73aacb2aa7312a281838b322d48e4382adbf741c
Joshua Owens <joshua.owens@fastmail.com>
2024-12-17 20:20:50 +0100

    BREAKING CHANGE: modify schema in src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -323,3 +323,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2024-10-23T20:18:15 – NIwSlk2TUMmerFVvJmvV
 // 2024-11-13T00:21:37 – ifru9LNaVN4IdzibIQPd
 // 2024-11-18T19:12:27 – 94j4SpzevzdlBJEk4uj4
+// 2024-12-17T20:20:50 – og0sTWou3ZThj07PhxnE
