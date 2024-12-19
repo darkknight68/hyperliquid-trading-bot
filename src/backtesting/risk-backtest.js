c031dfb3ca086a8eb324e805793643fc71df9be1
Christy Jenkins <christy.jenkins@posteo.net>
2024-12-19 11:33:42 +0100

    fix(src/backtesting/risk-backtest.js): address issue in error flow

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -708,3 +708,4 @@ module.exports = RiskAwareBacktester
 // 2024-11-29T01:12:10 – cb7NsMZkdAno7irgVawo
 // 2024-12-12T14:11:47 – IM3BGOzTbmfJLO9l1UhC
 // 2024-12-17T20:20:59 – jAUBx5UBGwMy47GJw5ZF
+// 2024-12-19T11:33:42 – KbekI05RUsiUHPis8gvE
