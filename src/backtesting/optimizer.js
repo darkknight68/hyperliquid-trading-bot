5ca09d34ac8ac7d1b3357e88b8e4ae603caff104
Mark Middleton <mark.middleton@tutanota.com>
2024-12-29 07:46:24 +0100

    refactor(AUTH-201): isolate auth logic in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -637,3 +637,4 @@ module.exports = StrategyOptimizer
 // 2024-11-10T12:12:10 – 1PHMe9da2R82k0tnxyjx
 // 2024-11-30T14:48:07 – KC3mdc31TNjlnUIM9of4
 // 2024-12-28T02:34:53 – wQlcQFznNlZ1KusdQbC9
+// 2024-12-29T07:46:24 – RALfvP1V4mM6P52qKc37
