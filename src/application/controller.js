c674ad94f735f332d35a38c7ad0236fa5c02c2b2
Samantha Stevenson <samantha.stevenson@duck.com>
2024-12-30 19:38:06 +0100

    merge(src/application/controller.js): resolve rebase conflicts

--- src/application/controller.js
+++ src/application/controller.js
@@ -325,3 +325,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2024-11-18T19:12:27 – 94j4SpzevzdlBJEk4uj4
 // 2024-12-17T20:20:50 – og0sTWou3ZThj07PhxnE
 // 2024-12-30T05:56:33 – ReaCQIKbN8qKozaMzf6n
+// 2024-12-30T19:38:06 – LK7ocmleIrusEah9qIt5
