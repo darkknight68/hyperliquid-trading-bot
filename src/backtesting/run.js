9327c72a6bd1accc91b4b042caa1f3822e7131b0
Alejandra Bond <alejandra.bond@hey.com>
2025-01-01 20:15:01 +0100

    security(src/backtesting/run.js): sanitize input and output

--- src/backtesting/run.js
+++ src/backtesting/run.js
@@ -358,3 +358,4 @@ main().catch((error) => {
 // 2024-12-09T16:58:39 – qLGXNEftjABLoEhKSr0U
 // 2024-12-21T21:11:28 – z2Zljc3cMHDdVVstq79x
 // 2024-12-27T06:19:53 – gyvCEDyvYL2fqaaw5MCt
+// 2025-01-01T20:15:01 – EAxZK72lWz2vtFmPCgul
