c00363afedf7497434f22cfd55d84865a4f61170
David Shea <david.shea@fastmail.com>
2025-01-22 10:05:56 +0100

    Remove unused function from src/backtesting/visualization.js

--- src/backtesting/visualization.js
+++ src/backtesting/visualization.js
@@ -805,3 +805,4 @@ module.exports = BacktestVisualizer
 // 2024-10-28T05:12:02 – rsAT3yBUoRPdAGfLHkuN
 // 2024-10-30T18:13:51 – HmLBDnkg7cv9SyWx8Zsd
 // 2025-01-18T22:08:35 – GFuAkR6J8RGULJawJVGZ
+// 2025-01-22T10:05:56 – r2iK8239JR9rQdvIZqSK
