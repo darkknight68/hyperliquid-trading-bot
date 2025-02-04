e96b4969b3bf1c3743bf19b6e5c34c70c7dcdaae
Mrs. Crystal Patel <mrs..crystal.patel@gmail.com>
2025-02-04 04:58:28 +0100

    Fix edge case in src/hyperliquid/websocket.js

--- src/hyperliquid/websocket.js
+++ src/hyperliquid/websocket.js
@@ -216,3 +216,4 @@ module.exports = { testWebSocket }
 // 2025-01-17T18:11:53 – LYnduEC8jzM9o2CTddYF
 // 2025-01-25T18:02:48 – fpvse8OZFYgqjBxRL1Nz
 // 2025-01-29T22:05:27 – LVIfg3dzv51fcnbiGGcR
+// 2025-02-04T04:58:28 – bJNBQtMNbI2uxUU4ST9N
