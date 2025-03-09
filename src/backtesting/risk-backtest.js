11403f5c50bccdbe4a29b66cabd6c229c0248a04
Nicole Thomas <nicole.thomas@pm.me>
2025-03-09 19:12:13 +0100

    Add helper function to src/backtesting/risk-backtest.js

--- src/backtesting/risk-backtest.js
+++ src/backtesting/risk-backtest.js
@@ -716,3 +716,4 @@ module.exports = RiskAwareBacktester
 // 2025-02-09T19:41:54 – jpFTzxpkLXYkMgJqJ7JM
 // 2025-03-02T16:56:08 – 5BJQbpEGCj5x2pPGVPDK
 // 2025-03-03T15:38:10 – bK7i2BWCIQQBxHmq8Zrf
+// 2025-03-09T19:12:13 – VLZ8on4alTyhiQzETLT9
