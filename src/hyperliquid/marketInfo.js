b51d35e9e9adaab4db538039aa0b27cab67ba8c1
Amy Branch <amy.branch@hushmail.com>
2025-03-15 04:26:32 +0100

    temp(src/hyperliquid/marketInfo.js): experimental changes (to be removed)

--- src/hyperliquid/marketInfo.js
+++ src/hyperliquid/marketInfo.js
@@ -394,3 +394,4 @@ module.exports = {
 // 2025-02-25T22:49:03 – KQ88v4M5K4tzxW8GOnKj
 // 2025-03-02T20:54:09 – Wyl9dp9WllaFwBeWgzid
 // 2025-03-02T23:16:02 – bGHfruUo9wABaougkVD5
+// 2025-03-15T04:26:32 – GTjJBDEyQacejz4WwXZJ
