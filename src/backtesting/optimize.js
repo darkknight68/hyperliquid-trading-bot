fa3a4254c13b25dd77bbf3c61ce61e6b2f81d12a
Laura Johnson <laura.johnson@fastmail.com>
2025-03-16 17:56:45 +0100

    Change default behavior in src/backtesting/optimize.js

--- src/backtesting/optimize.js
+++ src/backtesting/optimize.js
@@ -267,3 +267,4 @@ main().catch((error) => {
 // 2025-02-14T01:59:32 – 9lV4YiBxWBQA30q6LloP
 // 2025-02-26T15:59:39 – 3b4KOUayjyKDEF8jDXW0
 // 2025-03-08T13:50:45 – mYugqVoyBDFwHpu6a7B1
+// 2025-03-16T17:56:45 – cKNGI0pk5KVrqiLoqybL
