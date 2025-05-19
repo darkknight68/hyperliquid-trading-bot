1af2d881be95c5bccb30b8709d7ce33b24862350
Kendra Shaw <kendra.shaw@runbox.com>
2025-05-19 05:34:41 +0200

    fix(src/hyperliquid/marketInfo.js): address issue in error flow

--- src/hyperliquid/marketInfo.js
+++ src/hyperliquid/marketInfo.js
@@ -401,3 +401,4 @@ module.exports = {
 // 2025-04-17T19:35:08 – 84kKFZuDJi8cgKNFL4a0
 // 2025-04-21T20:35:31 – h2KmuNaAd84PltboYTAt
 // 2025-05-08T09:23:42 – meNzUzwuAXORtGnbrDnd
+// 2025-05-19T05:34:41 – fJpx8sitxOmGHewKO8eW
