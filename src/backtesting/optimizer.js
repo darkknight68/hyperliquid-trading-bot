46431625a860bbd61bd1c2eafe4c881136fe8346
Juan Medina <juan.medina@runbox.com>
2025-05-24 11:27:38 +0200

    feat(DASH-88): add dashboard filters in src/backtesting/optimizer.js

--- src/backtesting/optimizer.js
+++ src/backtesting/optimizer.js
@@ -645,3 +645,4 @@ module.exports = StrategyOptimizer
 // 2025-04-02T06:07:46 – mdhGYfQSdGLGloVRUbcw
 // 2025-04-04T12:04:35 – vrVz8jsTLMwKsN6U8iOm
 // 2025-04-30T19:44:55 – lRFwtfM0Gy3N3EtneOx7
+// 2025-05-24T11:27:38 – ixD21niD2JB2ZOpblwP9
