63cb1209e1f7ba1a4d617eeb16401df01a6dec87
Theresa Green <theresa.green@posteo.net>
2025-06-23 00:35:32 +0200

    Remove deprecated code from src/application/controller.js

--- src/application/controller.js
+++ src/application/controller.js
@@ -339,3 +339,4 @@ main(symbol, interval, leverage, leverageMode, positionSize)
 // 2025-06-02T11:15:24 – YCXd84DFvdvdFQOvntcJ
 // 2025-06-07T05:27:03 – ns2673aQLnkb1ebHIGFK
 // 2025-06-07T19:36:24 – jgjWLmaK7LkEA37NC5bW
+// 2025-06-23T00:35:32 – 7PxaotzrfXZPW3Ag4bPn
